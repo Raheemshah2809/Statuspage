74a2b2460d698b801d0dd58a1f13ed1e79005160
raheem <raheemshah2809@gmail.com>
2022-06-22 17:25:40 +0100

    version 2.0 beta

--- main.js
+++ main.js
@@ -1,17 +1,54 @@
-function updateClock() {
-    var now = new Date(), // current date
-        months = ['January', 'February', 'March','April', 'May ','June','July','August','September','October','November','December'];
-        time = now.getHours() + ':' + now.getMinutes(), // again, you get the idea
 
-        // a cleaner way than string concatenation
-        date = [ months[now.getMonth()],
-                now.getDate(),
-                now.getFullYear()].join(' ');
+$(document).ready(function () {
+    function displayTime() {
 
-    // set the content of the element with the ID time to the formatted string
-    document.getElementById('time').innerHTML = [date, time].join(' ');
+        var currentTime = new Date();	
+        var hours = currentTime.getHours();
+        var minutes = currentTime.getMinutes();
+        var seconds = currentTime.getSeconds();
+        var meridiem = " AM";
 
-    // call this function again in 1000ms
-    setTimeout(updateClock, 1000);
-}
-updateClock(); // initial call
+        if (hours > 11) {
+            hours = hours - 12;
+            meridiem = " PM";
+        }
+        if (hours === 0) {
+            hours = 12;
+        }
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        $("#clock").text(hours + ":" + minutes + ":" + seconds + meridiem);
+    }
+
+    function displayDay() {
+        var currentDay = new Date();
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var day = days[currentDay.getDay()];
+        $("#day").text(day);
+    }
+
+    function displayDate() {
+        var currentDate = new Date();
+        var year = currentDate.getFullYear();
+        var date = currentDate.getDate();
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var month = months[currentDate.getMonth()];
+        if (date < 10) {
+            date = "0" + date;
+        }
+
+        $("#date").text(month + " " + date + " " + year);
+    }
+    displayTime();
+    setInterval(displayTime, 1000);
+    displayDay();
+    displayDate();
+
+});
